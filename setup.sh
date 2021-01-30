7669a2350c8c8f9f98384c5ef2c083ca75dd50ad
Luis Garcia <luis.a.garcia@hpe.com>
2021-01-30 12:18:55 -0600

    updating setup.sh

--- setup.sh
+++ setup.sh
@@ -1,9 +1,7 @@
 #!/bin/bash
 
-git clone https://github.com/holman/dotfiles.git ~/.dotfiles
-cd ~/.dotfiles
-
 ln -s $HOME/.dotfiles/tmux/tmux.conf $HOME/.tmux.conf
 ln -s $HOME/.dotfiles/emacs/emacs.conf $HOME/.emacs
 ln -s $HOME/.dotfiles/emacs/emacs.d $HOME/
 
+cp  $HOME/.dotfiles/bash/env.sh /etc/profile.d/00-env.sh
