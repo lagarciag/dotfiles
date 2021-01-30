55110a4874d752257a93885e780bc6414a4f32ec
Luis Garcia <luis.a.garcia@hpe.com>
2021-01-30 12:01:26 -0600

    updating setup

--- setup.sh
+++ setup.sh
@@ -5,5 +5,5 @@ cd ~/.dotfiles
 
 ln -s $HOME/.dotfiles/tmux/tmux.conf $HOME/.tmux.conf
 ln -s $HOME/.dotfiles/emacs/emacs.conf $HOME/.emacs
-ln -s $HOME/.dotfiles/emacs/emacs.d $HOME/.emacs.d
+ln -s $HOME/.dotfiles/emacs/emacs.d $HOME/
 
