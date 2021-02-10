453819dd05dcf7f7e69d299cd553e6f042fbfe2b
Luis Garcia <luis.a.garcia@hpe.com>
2021-02-10 13:31:44 -0600

    adding clion installation

--- vagrant/hack.sh
+++ vagrant/hack.sh
@@ -11,9 +11,9 @@ sudo apt-get install emacs-nox libxtst6 libxi6 libnss3 libxrandr2 -y
 
 echo "alias clion=~/.config/clion-2020.3.2/bin/clion.sh" >> ~/.bashrc
 
-FILE=~/.emacs
-if test -f "$FILE"; then
-    echo "$FILE exists."
+EMACS=~/.emacs
+if test -f "$EMACS"; then
+    echo "$EMACS exists."
 else
     echo "alias e=emacs" >> ~/.bashrc
     echo "export EDITOR=emacs" >> ~/.bashrc
@@ -23,3 +23,14 @@ fi
 
 ln -sf $HOME/.config/dotfiles/tmux/tmux.conf $HOME/.tmux.conf
 
+CLION_VERSION="2020.3.2"
+CLION=~/.config/clion-${CLION_VERSION}/bin/clion.sh
+CLION_URL=https://download.jetbrains.com/cpp/CLion-${CLION_VERSION}.gz?_ga=2.227410653.1384653249.1612985181-944089105.1610581540
+if test -f "$CLION"; then
+    echo "found Clion installation"
+else
+    echo "installing c lion"
+    wget $CLION_URL
+    tar -xvzf CLion-${CLION_VERSION}.tar.gz ~/.config/
+    rm CLion-${CLION_VERSION}.tar.gz
+fi
