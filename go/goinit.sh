17cf2fcd9bd366461befc6f0213fc0643b643e4f
Luis Garcia <luis.a.garcia@hpe.com>
2021-02-11 10:50:11 -0600

    adding several go initializers

---
+++ go/goinit.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+set -e
+
+go get -u github.com/spf13/cobra
+go get github.com/spf13/cobra/cobra
+go get github.com/spf13/viper
+go get github.com/sirupsen/logrus
+go get github.com/juju/errors
+go get golang.org/x/tools/cmd/goimports
+go get github.com/stretchr/testify
+go get github.com/stretchr/testify/assert
+cp ~/.dotfiles/go/.golangci.yaml .golangci.yaml
+
+mkdir -p ./.idea
+cp ~/.dotfiles/go/watcherTasks.xml ./.idea/
+# binary will be $(go env GOPATH)/bin/golangci-lint
+sudo curl -sSfL https://raw.githubusercontent.com/golangci/golangci-lint/master/install.sh | sudo sh -s -- -b /usr/local/bin v1.36.0
+
+golangci-lint --version
